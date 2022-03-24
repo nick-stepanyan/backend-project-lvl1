84297cab3fd9eb81b8738c257efac4dc739305ea
gisma_gismo <gisma4x4@mail.ru>
2022-03-24 18:47:56 +0300

    even game init release

---
+++ bin/brain-even.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import answer, { askName } from '../src/cli-even.js';
+
+console.log('Welcome to the Brain Games!');
+const name = askName();
+console.log(`Hello, ${name}!`);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const answ = answer();
+console.log(answ);
---
+++ src/cli-even.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+export const askName = () => readlineSync.question('May I have your name?: ');
+export default () => {
+  const name = askName();
+  let lap = 3; // number of rounds
+  let result = '';
+  while (lap > 0) {
+    const number = Math.floor(Math.random() * (100)); // number generating
+    console.log(number);
+    const answer = readlineSync.question('Your answer: '); // answer
+    lap -= 1;
+    if ((number % 2 === 0 && answer === 'yes') || (number % 2 !== 0 && answer === 'no')) {
+      console.log('Correct!');
+    } else if (answer === 'yes') {
+      result = `yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${name}!`;
+      break;
+    } else if (answer === 'no') {
+      result = `'no' is wrong answer ;(. Correct answer was 'yes'.Let's try again, ${name}!`;
+      break;
+    } else {
+      result = `It is a wrong answer, try again, ${name}!`;
+      break;
+    }
+  }
+  return result;
+};
