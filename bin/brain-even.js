c590872b2f75d36a963d9cefab544618fa9007d7
gisma_gismo <gisma4x4@mail.ru>
2022-04-01 15:57:57 +0300

    some logic  fixes

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
+import { cons } from '@hexlet/pairs';
 import { greeting, sayRules, resultComplain } from '../src/index.js';
-import arr from '../src/cli-even.js';
 
 greeting();
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 sayRules(rule);
-resultComplain(arr);
+const arrGenerate = () => {
+  const taskArray = [];
+  for (let i = 0; i < 3; i += 1) {
+    const number = String(Math.floor(Math.random() * (100)));
+    const answer = (number % 2 === 0) ? 'yes' : 'no';
+    taskArray.push(cons(number, answer));
+  }
+  return taskArray;
+};
+resultComplain(arrGenerate());
