0b90e3161ba1e337889c1ae84f4fcab580c0349b
gisma_gismo <gisma4x4@mail.ru>
2022-04-01 19:30:41 +0300

    del cli-even

--- src/cli-even.js
+++ src/cli-even.js
@@ -1,13 +1,13 @@
-import { cons } from '@hexlet/pairs';
+// import { cons } from '@hexlet/pairs';
 
-const arrGenerate = () => {
-  const taskArray = [];
-  for (let i = 0; i < 3; i += 1) {
-    const number = String(Math.floor(Math.random() * (100)));
-    const answer = (number % 2 === 0) ? 'yes' : 'no';
-    taskArray.push(cons(number, answer));
-  }
-  return taskArray;
-};
+// const arrGenerate = () => {
+// const taskArray = [];
+// for (let i = 0; i < 3; i += 1) {
+// const number = String(Math.floor(Math.random() * (100)));
+// const answer = (number % 2 === 0) ? 'yes' : 'no';
+// taskArray.push(cons(number, answer));
+// }
+// return taskArray;
+// };
 
-export default arrGenerate();
+// export default arrGenerate();
